24ab2ba916349eb094e05ac664a2f00e6bfddab2
Mike Allanson <michael.allanson@gmail.com>
2018-06-25 12:58:07 +0100

    Add @jlengstorf's non-blocking CSS loader for the docsearch CSS

---
+++ www/gatsby-browser.js
@@ -0,0 +1,11 @@
+exports.onClientEntry = () => {
+  // non-blocking CSS load
+  (function() {
+    const path = `https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css`
+    const link = document.createElement(`link`)
+    link.setAttribute(`rel`, `stylesheet`)
+    link.setAttribute(`type`, `text/css`)
+    link.setAttribute(`href`, path)
+    document.head.appendChild(link)
+  })()
+}
